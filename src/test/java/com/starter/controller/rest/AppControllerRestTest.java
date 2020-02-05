30bbd9b8838c29b543221ff40785223481d22e16
aymensoual <aymensoual@users.noreply.github.com>
2020-02-05 15:57:40 +0100

    initial commit

---
+++ src/test/java/com/starter/controller/rest/AppControllerRestTest.java
@@ -0,0 +1,35 @@
+package com.starter.controller.rest;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starter.entity.Employee;
+import com.starter.services.AppServiceImplementation;
+
+@RunWith(SpringRunner.class)
+public class AppControllerRestTest {
+
+
+}
